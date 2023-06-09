d84a0723e6bf0fc9548e606b62c4fe08f4eb45e7
ihsanabuhanifah <ihsanabuhanifah@gmail.com>
2023-06-10 06:28:46 +0700

    update

---
+++ components/Loading.jsx
@@ -0,0 +1,15 @@
+'use client'
+import React from "react";
+import { Spinner } from "@chakra-ui/react";
+function Loading({ size = "md" }) {
+  return (
+    <Spinner
+      size={size}
+      thickness="3px"
+      color="blue.400"
+      emptyColor="gray.200"
+    ></Spinner>
+  );
+}
+
+export default Loading;
